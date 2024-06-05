1833696d58e43d0a9089950c5fb14552e7461d8d
Emre kavcakar <kavcakaremre@gmail.com>
2024-06-05 02:19:25 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/movies/")
+      .then((response) => {
+        setMovies(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the movies!", error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie List</h1>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            {movie.title} - {movie.description}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
